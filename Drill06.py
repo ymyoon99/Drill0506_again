34b8ae3b4f389f43c709c525fe26c55b02d61efe
ymyoon(OMEN) <ozone753@naver.com>
2023-10-04 18:03:56 +0900

    클릭한 마우스 위치에 손 생성.

--- Drill06.py
+++ Drill06.py
@@ -22,6 +22,8 @@ def handle_events():
             running = False
         elif event.type == SDL_MOUSEMOTION:
             mx, my = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            points.append((event.x, TUK_HEIGHT - 1 - event.y)) # 클릭된 위치를 새로운 점으로 추가.
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -41,7 +43,7 @@ def reset_world():
     frame = 0
     action = 3
 
-    points = [ (100, 900), (1200, 800), (500, 100)]
+    points = [ ]
     set_new_target_arrow()
 
 
