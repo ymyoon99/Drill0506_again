321b59875dd4d5a58bd2ea11f04410bc8a54ada5
ymyoon(OMEN) <ozone753@naver.com>
2023-10-04 16:41:53 +0900

    arrow image load & draw on canvas

--- Drill05.py
+++ Drill05.py
@@ -1,12 +1,17 @@
 from pico2d import *
+import random
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 
 
 def load_resources():  # 함수와 함수 사이에 두줄이 권장
     global TUK_ground, character
+    global arrow
+
+    arrow = load_image('hand_arrow.png')
     TUK_ground = load_image('TUK_GROUND.png')
     character = load_image('animation_sheet.png')
+
 
 
 def handle_events():
@@ -15,6 +20,7 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -22,14 +28,19 @@ def handle_events():
 
 def reset_world():
     global running, x, y, frame
+    global hx, hy
+
     running = True
     x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
+
+    hx, hy = TUK_WIDTH - 50, TUK_HEIGHT - 50
 
 
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    arrow.draw(hx, hy)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
 
