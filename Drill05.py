f02d66513f1a72c96c0caa3c9be0844a61cb342c
ymyoon(OMEN) <ozone753@naver.com>
2023-10-04 16:23:44 +0900

    캐릭터 마우스 이동 기능 삭제

--- Drill05.py
+++ Drill05.py
@@ -3,7 +3,7 @@ from pico2d import *
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 
 
-def load_resources():  # 함수와 함수 사시에 두줄이 권장
+def load_resources():  # 함수와 함수 사이에 두줄이 권장
     global TUK_ground, character
     TUK_ground = load_image('TUK_GROUND.png')
     character = load_image('animation_sheet.png')
@@ -11,13 +11,10 @@ def load_resources():  # 함수와 함수 사시에 두줄이 권장
 
 def handle_events():
     global running
-    global x, y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
