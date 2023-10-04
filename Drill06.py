f836f7e71ed16008e587d9ff0f9dc58661bebb83
ymyoon(OMEN) <ozone753@naver.com>
2023-10-04 17:25:28 +0900

    손 화살표가 마우스인 기능 구현 완료

--- Drill06.py
+++ Drill06.py
@@ -15,11 +15,13 @@ def load_resources():  # 함수와 함수 사이에 두줄이 권장
 
 def handle_events():
     global running
+    global mx, my
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-
+        elif event.type == SDL_MOUSEMOTION:
+            mx, my = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -29,6 +31,9 @@ def reset_world():
     global running, cx, cy, frame
     global t
     global action
+    global mx, my
+
+    mx, my = 0, 0
 
     running = True
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
@@ -53,7 +58,7 @@ def set_new_target_arrow():
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    # arrow.draw(hx, hy)
+    arrow.draw(mx, my)
     character.clip_draw(frame * 100, 100 * action, 100, 100, cx, cy)
     update_canvas()
 
